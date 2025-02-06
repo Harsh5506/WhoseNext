38747826be99fa743010ce4b641137ea8950050a
harsh5506 <harsh5506@gmail.com>
2025-02-06 23:36:31 +0530

    Server Added

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
-let pass = prompt("Enter Secret Word");
-
-if(pass=="arise"){
-    document.getElementById("menu-toggle").addEventListener("click", function() {
-        const menu = document.getElementById("button-menu");
-        menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-    });
-    
-    const englishLetters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    const gujaratiLetters = "અઆઇઈઉઊએઐઓઔકખગઘચછજઝટઠડઢતથદધનપફબભમયરલવશષસહ".split("");
-    const numbers = "0123456789".split("");
-    
-    let englishPool = [...englishLetters];
-    let gujaratiPool = [...gujaratiLetters];
-    let numberPool = [...numbers];
-    
-    function generateCharacter(type) {
-        let pool;
-        if (type === 'english') pool = englishPool;
-        else if (type === 'gujarati') pool = gujaratiPool;
-        else if (type === 'number') pool = numberPool;
-    
-        if (pool.length === 0) {
-            document.getElementById("letter").innerText = "All used!";
-            return;
-        }
-    
-        const randomIndex = Math.floor(Math.random() * pool.length);
-        const randomChar = pool.splice(randomIndex, 1)[0];
-        
-        document.getElementById("letter").innerText = randomChar;
-    }
-    
-    function resetCharacters() {
-        englishPool = [...englishLetters];
-        gujaratiPool = [...gujaratiLetters];
-        numberPool = [...numbers];
-        document.getElementById("letter").innerText = "👍";
-    }
-}
+const express = require("express");
+const app = express();
+const path = require("path");
+app.listen(3000, () => {
+    console.log("app is listening");
+});
+app.get("/WhoseNext", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
